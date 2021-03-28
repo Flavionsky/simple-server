d58fa7e06221858ccffd77b7d301acbe294b2a20
Flavio Muscente <flaviomuscente@libero.it>
2021-03-28 19:09:18 +0200

    add interval on mousedown

--- src/components/Custom.vue
+++ src/components/Custom.vue
@@ -4,28 +4,19 @@
       <li v-for="(element, index) in elements" :key="index">
         <h3>{{ element.name }}</h3>
         <p>
-          quantit&#224;: {{ element.eachqty }}
+          quantit&#224;: {{ element.qty + element.min }}
           <span v-if="element.name == 'hd'"
-            >GB <i @click="addItem(element)">+10 GB</i
-            ><i @click="minusQty(element)">-10 GB</i></span
+            >GB <button @click="addItem(element)" @mousedown="delayUp(element)" @mouseup="stop()" @mouseleave="stop()">+10 GB</button>
+            <button @click="minusQty(element)" @mousedown="delayDown(element)" @mouseup="stop()" @mouseleave="stop()">-10 GB</button></span
           >
           <span v-else
-            ><i @click="addItem(element)">+ 1</i
-            ><i @click="minusQty(element)">-1</i></span
+            ><button @click="addItem(element)" @mousedown="delayUp(element)" @mouseup="stop()" @mouseleave="stop()">+1</button>
+            <button @click="minusQty(element)" @mousedown="delayDown(element)" @mouseup="stop()" @mouseleave="stop()">-1</button></span
           >
         </p>
         <p>prezzo: {{ element.price }} &#8364;</p>
       </li>
     </ul>
-    <div v-if="selectedElements.length">
-      <div v-for="(selectedEl, index) in selectedElements" :key="index">
-        <h3>{{ selectedEl.name }}</h3>
-        <p>
-          {{ selectedEl.qty + selectedEl.min }}
-        </p>
-      </div>
-      <p>totale: {{ total() }} &#8364;</p>
-    </div>
   </div>
 </template>
 
@@ -38,51 +29,52 @@ export default {
         {
           id: 1,
           name: "cpu",
-          qty: 1,
+          qty: 0,
           eachqty: 1,
           price: 20,
           min: 1,
           max: 7,
+          total: 0
         },
         {
           id: 2,
           name: "ram",
-          qty: 1,
+          qty: 0,
           eachqty: 1,
           price: 3,
           min: 1,
           max: 15,
+          total: 0
         },
         {
           id: 3,
           name: "hd",
-          qty: 10,
+          qty: 0,
           eachqty: 10,
           price: 0.35,
           min: 50,
           max: 450,
+          total: 0
         },
         {
           id: 4,
           name: "ip",
-          qty: 1,
+          qty: 0,
           eachqty: 1,
           price: 5,
           min: 1,
           max: 4,
+          total: 0
         },
       ],
-      selectedElements: [],
+      timeout: null
     };
   },
   methods: {
     addItem: function (element) {
-      if (this.selectedElements.length < 1) {
-        this.pushData(element);
-      } else {
-        var isInArray = this.selectedElements.find((e) => e.id === element.id);
+        var isInArray = this.elements.find((e) => e.id === element.id);
         if (isInArray) {
-          this.selectedElements.forEach(function (e) {
+          this.elements.forEach(function (e) {
             if (e.id === element.id && e.id != 3) {
               if (e.qty < e.max) {
                 e.qty += e.eachqty;
@@ -96,29 +88,29 @@ export default {
               }
             }
           });
-        } else {
-          this.pushData(element);
         }
-      }
+      
     },
-    pushData: function (element) {
-      this.selectedElements.push({
-        id: element.id,
-        name: element.name,
-        qty: element.qty,
-        price: element.price,
-        eachqty: element.eachqty,
-        max: element.max,
-        min: element.min,
-        total: element.price,
-      });
-    },
+    delayUp (element) {
+        // Re-enable after 5 seconds
+        this.timeout = setInterval(() => {
+        this.addItem(element);
+        }, 150)
+
+      },
+      delayDown (element) {
+        // Re-enable after 5 seconds
+        this.timeout = setInterval(() => {
+        this.minusQty(element);
+        }, 150)
+
+      },
     removeItem: function (element) {
-      var index = this.selectedElements.indexOf(element);
-      this.selectedElements.splice(index, 1);
+      var index = this.elements.indexOf(element);
+      this.elements.splice(index, 1);
     },
     minusQty: function (element) {
-      this.selectedElements.forEach(function (e) {
+      this.elements.forEach(function (e) {
         if (e.id === element.id && e.id != 3) {
           if (e.qty >= e.min) {
             e.qty -= e.eachqty;
@@ -139,11 +131,16 @@ export default {
     },
     total: function () {
       var sum = 80;
-      this.selectedElements.forEach(function (element) {
+      this.elements.forEach(function (element) {
         sum += parseFloat(element.total);
       });
       return sum.toFixed(2);
     },
+   stop() {
+     // clear the timeout before the component is destroyed
+     clearInterval(this.timeout)
+     this.timeout = null;
+    }
   },
 };
 </script>
